3c9812e6bd40aee90955d62e5ce906353c78581c
Akil Khatri <khatriakil104@gmail.com>
2025-05-08 18:43:58 +0530

    create basic login/signup ui

---
+++ src/db/db.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI || "");
+        const connection = mongoose.connection
+
+        connection.on("connected", () => {
+            console.log("Connected to MongoDB");
+        })
+
+        connection.on("error", (err) => {
+            console.log("MongoDB connection error", err);
+            process.exit();
+        })
+    } catch (error) {
+        console.error("Something went wrong:",error);
+    }
+}
